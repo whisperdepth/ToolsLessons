9e65d7bd89c73e9f3124d423d09513a6e24f3974
Roman Marchenko <whisperdepth@gmail.com>
2020-06-06 16:39:13 +0300

    d

---
+++ lesson5/task2/src/profile/gateway.js
@@ -0,0 +1,9 @@
+// file path: src/profile/gateway.js
+
+export async function fetchUser(userId) {
+  const response = await fetch(`https://api.github.com/users/${userId}`);
+  if (!response.ok) {
+    throw new Error("Failed to get user data");
+  }
+  return await response.json();
+}
