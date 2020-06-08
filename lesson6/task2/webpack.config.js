11a350176ca16c6606b9ba8043685654718b7923
Roman Marchenko <whisperdepth@gmail.com>
2020-06-08 14:34:57 +0300

    g

---
+++ lesson6/task2/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+};
